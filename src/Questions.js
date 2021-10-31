3fd9b75dfe5dc2d30b663ab1cb48781ae26acae2
viktor <vikhulei@gmail.com>
2021-10-31 20:34:03 +0200

    Second ten added

--- src/Questions.js
+++ src/Questions.js
@@ -14,8 +14,18 @@ const Questions = {
   ],
 
   secondTen: [
-    {q: 'Слів ще нема', a: 'No words', tag:0},
+    {q: 'Ти', a: 'You', tag:0},
+    {q: 'Я йду додому', a: 'I go home', tag:0},
+    {q: 'Тобі подобається ця їжа?', a: 'Do you like this food?', tag:0},
+    {q: 'Коли ти зможеш прогулятися зі мною?', a: 'When can you walk with me?', tag:0},
+    {q: 'Я зможу погратися ввечері сьогодні', a: 'I can play in the evening today', tag:0},
+    {q: 'Моя мама їздить на машині', a: 'My Mum drives a car', tag:0},
+    {q: 'Мені подобається наша школа', a: 'I like our school', tag:0},
+    {q: 'Цей мультфільм дуже класний', a: 'This cartoon is very cool', tag:0},
+    {q: 'Я вмію кататися на велосипеді', a: 'I can ride a bike', tag:0},
+    {q: 'Суші - моя улюблена їжа', a: 'Sushi is my favorite food', tag:0},
   ],
+
   thirdTen: [
     {q: 'Слів ще нема', a: 'No words', tag:0},
   ],
